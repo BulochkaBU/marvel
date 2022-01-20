b35557a10b59ad4abbba2135b1e0a823c0f62b04
Anastasia <nastyaerm@mail.ru>
2022-01-20 16:59:40 +0300

    create random char

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
-import MarvelService from './services/MarvelService';
 
-const marvelService = new MarvelService();
 
-marvelService.getCharacter(1017100).then(res => res.data.results.forEach(element => {console.log(element.name)}))
 
 
 ReactDOM.render(
